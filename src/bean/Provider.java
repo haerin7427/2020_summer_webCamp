5c2f192d70d2d8b00e45816ce2c02c9893a5f6ee
haelingim <haelingim@218.38.137.27>
2020-08-08 21:56:50 +0900

    heroku + registerform

---
+++ src/bean/Provider.java
@@ -0,0 +1,8 @@
+package bean;
+
+public interface Provider {
+	String DRIVER="com.mysql.jdbc.Driver";  
+	String CONNECTION_URL="jdbc:mysql://www.db4free.net:3306/haerindb";  
+	String USERNAME="haerin7427";  
+	String PASSWORD="hgu217196"; 
+}
